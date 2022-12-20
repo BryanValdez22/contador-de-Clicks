b514423b03e98db63515eab448daacfab47176d8
Brayan Valdez Jaraba <jaravaldezbryan2016@gmail.com>
2022-12-20 17:01:37 -0500

    proyecto Click

---
+++ src/components/contador.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Contador = () => {
+
+    const [contador, setContador] = useState(0);
+
+    function incrementar(){
+        setContador(contador + 1);
+    }
+
+    function removerContador(){
+        setContador(0)
+    }
+
+    return (
+        <div className='card'>
+            <h2 className='h2'>Contador de Clicks</h2>
+            <h3> {contador} </h3>
+            <button className='btn' onClick={incrementar}>Click</button>
+            <button className='btn' onClick={removerContador}>Reiniciar</button>
+        </div>
+    );
+}
+
+export default Contador;
